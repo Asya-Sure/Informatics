7a6b48ed5808def39c6e849d7808bbbf56d1db02
Asya-Sure <gorbatchovaasa123@gmail.com>
2021-01-20 20:53:19 +0200

    Add PR23

---
+++ PR23_individ/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+    Private Sub button_Click(sender As Object, e As EventArgs) Handles button.Click
+
+        Dim numberMore = TextBoxMore.Text, numberLess = TextBoxLess.Text, counter As Integer
+        For counter = numberMore To numberLess
+            If Not counter Mod 2 = 0 Then
+                TextBoxResult.Text = TextBoxResult.Text + " " + counter.ToString
+            End If
+
+
+        Next counter
+
+    End Sub
+End Class
